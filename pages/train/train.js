053ce6a3832dade5ffd150230ba39c54fed27adc
xiaofan <1353815633@qq.com>
2018-09-07 16:26:59 +0800

    火车票首页

--- pages/train/train.js
+++ pages/train/train.js
@@ -2,6 +2,7 @@
 Page({
 
   data: {
+    currentTab: 0,
     imgUrls: [
       '../../images/haibao/1.jpg',
       '../../images/haibao/2.jpg',
@@ -12,4 +13,26 @@ Page({
   onLoad: function (options) {
   
   },
+
+  switchNav: function(e) {
+    let id = e.currentTarget.id;
+    
+    this.setData({
+      currentTab: id
+    });
+  },
+
+  formSubmit: function(e) {
+    let formValue = e.detail.value;
+    let startStation = formValue.startStation;
+    let endStation = formValue.endStation;
+    let date = formValue.date;
+    let week = formValue.week;
+    let gt = formValue.gt;
+    let xs = formValue.xs;
+
+    wx.navigateTo({
+      url: `../trainList/trainList?startStation=${startStation}&endStation=${endStation}&date=${date}&week=${week}&gt=${gt}&xs=${xs}`
+    })
+  }
 })
---
+++ pages/trainList/trainList.js
@@ -0,0 +1,12 @@
+// pages/trainList/trainList.js
+Page({
+  data: {
+  
+  },
+
+  onLoad: function (options) {
+    console.log(options);
+    
+  },
+
+})
