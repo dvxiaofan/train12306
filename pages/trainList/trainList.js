71a5496cc2a145923554a15bc9f5a09b49bdda5e
xiaofan <1353815633@qq.com>
2018-09-10 16:52:41 +0800

    火车列表页表头

--- pages/trainList/trainList.js
+++ pages/trainList/trainList.js
@@ -1,12 +1,19 @@
 // pages/trainList/trainList.js
 Page({
   data: {
-  
+    date: ''
   },
 
-  onLoad: function (options) {
-    console.log(options);
-    
+  onLoad: function (op) {
+    let startStation = op.startStation;
+    let endStation = op.endStation;
+    let date = op.date;
+    wx.setNavigationBarTitle({
+      title: startStation + ' → ' + endStation
+    });
+    this.setData({
+      date: date
+    });
   },
 
 })
